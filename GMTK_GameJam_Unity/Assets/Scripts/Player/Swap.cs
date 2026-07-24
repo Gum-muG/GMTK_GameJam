2d7d135857fce1b32453b698fddc5b1ffabd21e2
logngames <logngames@gmail.com>
2026-07-24 01:26:38 -0500

    SwapFix

--- GMTK_GameJam_Unity/Assets/Scripts/Player/Swap.cs
+++ GMTK_GameJam_Unity/Assets/Scripts/Player/Swap.cs
@@ -3,20 +3,29 @@ using UnityEngine;
 public class Swap : MonoBehaviour
 {
     public Transform counterpart;
-    private Transform temp;
+    public Transform forward;
+    public Rigidbody rb;
+    private Vector3 positionTemp; 
+    private Quaternion rotationTemp; 
 
     public void SwapPlaces()
     {
-        temp = transform;
-        transform.rotation = counterpart.rotation;
-        transform.position = counterpart.position;
-        counterpart.rotation = temp.rotation;
-        counterpart.position = temp.position;
+        
+        positionTemp = rb.position;
+        rotationTemp = rb.rotation;
+        Debug.Log(forward.rotation);
+        rb.rotation = counterpart.rotation;
+        rb.position = counterpart.position;
+        Debug.Log("Set Me");
+        counterpart.rotation = rotationTemp;
+        counterpart.position = positionTemp;
+        Debug.Log(forward.rotation);
+        Debug.Log("Set Counter");
     }
 
     public void Update()
     {
-        if (Input.GetKey(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T))
         {
             SwapPlaces();
         }
